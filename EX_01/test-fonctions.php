f48d35f1330abb4e45692baf064af20106c04b7e
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-06-30 10:35:28 +0200

    Ex 1 - Fonction Affichage 1

---
+++ EX_01/test-fonctions.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+
+    </head>
+
+    <body>
+        <?php
+            $nom_produit = "T-shirt femme";
+            $couleur = "rouge";
+            $prix = 15.50 ;
+            $disponible = true;
+            $disponible = false;
+            $quantité = "10";
+            ?> 
+
+    </body>
+</html> 
