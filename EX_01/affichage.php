aca3ed7c79d115c6a47c5026fa5c8d3808a4755a
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-06-30 15:18:55 +0200

    Ex 1 - Fonction Manipulation

--- EX_01/affichage.php
+++ EX_01/affichage.php
@@ -1,17 +1,36 @@
 <!DOCTYPE html>
 <html> 
     <head>
-
     </head>
 
     <body>
 
-        <h1><?php
-            function affichage ($afficher_titre) {
-                echo "Fiche produit : $afficher_titre";
-             }
-            affichage ("$nom_produit");
+        <?php
+            function afficher_titre ($nom_produit) {
+                echo "<h1> Fiche produit : $nom_produit </h1>";
+            }
+            affichage ("T-shirt femme");
+        ?>
+
+        <?php 
+           function afficher_description ( $couleur, $prix) {
+            echo "<p> Couleur : $couleur, Prix : $prix </p>";
+            }
+            afficher_description ("Rouge", 15.50);
+        ?>
+
+        <?php
+            function afficher_produit ($nom_produit, $couleur, $prix, $disponible) { 
+
+                if ($disponible==true) {
+                affichage_titre ();
+                affichage_description(); }
             
-        ?> </h1> 
+                else
+                {echo "<p> Le produit $nom_produit n'est malheuresement plus disponible.</p>"; }
+            }
+            affichage ("T-shirt femme", "Rouge", 15.50);
+        ?> 
+
     </body>
 </html> 
