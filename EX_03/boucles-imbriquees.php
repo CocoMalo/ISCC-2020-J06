c815982b1160019cf36016a60760d607970c0cd4
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-01 09:41:24 +0200

    Exo 3 - Boucles Imbriquées

---
+++ EX_03/boucles-imbriquees.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+    </head>
+
+    <body>
+        <?php
+            function clock() {
+                echo date ("h:i");
+             }
+        ?>
+    </body>
+</html>
